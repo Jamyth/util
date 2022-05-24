6d2ac5e029a33ac66cb5155fa079409f3cfa7606
Jamyth <jamyth0712@gmail.com>
2022-05-24 21:54:04 +0800

    [CHORE]: 1.1.7

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-export * from './core/ArrayUtil';
-export * from './core/DateUtil';
-export * from './core/DistributionGenerator';
-export * from './core/EnumUtil';
-export * from './core/ErrorUtil';
-export * from './core/HashUtil';
-export * from './core/IDGenerator';
-export * from './core/NumberUtil';
-export * from './core/ObjectUtil';
-export * from './core/PromiseUtil';
-export * from './core/RandomUtil';
-export * from './core/TextUtil';
-export * from './core/URLUtil';
-export * from './core/VersionComparator';
-export * from './decorator/Memo';
-export * from './decorator/Throttle';
-export * from './decorator/type';
-export * from './type';
+export * from "./core/ArrayUtil";
+export * from "./core/DateUtil";
+export * from "./core/DistributionGenerator";
+export * from "./core/EnumUtil";
+export * from "./core/ErrorUtil";
+export * from "./core/HashUtil";
+export * from "./core/IDGenerator";
+export * from "./core/NumberUtil";
+export * from "./core/ObjectUtil";
+export * from "./core/PromiseUtil";
+export * from "./core/RandomUtil";
+export * from "./core/TextUtil";
+export * from "./core/URLUtil";
+export * from "./core/VersionComparator";
+export * from "./decorator/Memo";
+export * from "./decorator/Throttle";
+export * from "./decorator/type";
+export * from "./type";
 
 /* eslint-disable @typescript-eslint/consistent-type-imports -- typesafe */
-export let ReactUtil: typeof import('./core/ReactUtil');
-export let useAPI: typeof import('./hooks/useAPI');
-export let useBool: typeof import('./hooks/useBool');
-export let useDidMountEffect: typeof import('./hooks/useDidMountEffect');
-export let useForceUpdate: typeof import('./hooks/useForceUpdate');
-export let usePrevious: typeof import('./hooks/usePrevious');
-export let useWhyDidYouUpdate: typeof import('./hooks/useWhyDidYouUpdate');
-export let useWillUnmountEffect: typeof import('./hooks/useWillUnmountEffect');
-export let BrowserUtil: typeof import('./web/BrowserUtil');
-export let ClipboardUtil: typeof import('./web/ClipboardUtil');
-export let LocalStorageUtil: typeof import('./web/LocalStorageUtil');
-export let SessionStorageUtil: typeof import('./web/SessionStorageUtil');
+export let ReactUtil: typeof import("./core/ReactUtil");
+export let useAPI: typeof import("./hooks/useAPI");
+export let useBool: typeof import("./hooks/useBool");
+export let useDidMountEffect: typeof import("./hooks/useDidMountEffect");
+export let useForceUpdate: typeof import("./hooks/useForceUpdate");
+export let usePrevious: typeof import("./hooks/usePrevious");
+export let useWhyDidYouUpdate: typeof import("./hooks/useWhyDidYouUpdate");
+export let useWillUnmountEffect: typeof import("./hooks/useWillUnmountEffect");
+export let BrowserUtil: typeof import("./web/BrowserUtil");
+export let ClipboardUtil: typeof import("./web/ClipboardUtil");
+export let LocalStorageUtil: typeof import("./web/LocalStorageUtil");
+export let SessionStorageUtil: typeof import("./web/SessionStorageUtil");
 /* eslint-enable @typescript-eslint/consistent-type-imports */
 
 // Only if window is defined || is browser environment
-if (typeof window !== 'undefined') {
-    BrowserUtil = require('./web/BrowserUtil');
-    ClipboardUtil = require('./web/ClipboardUtil');
-    LocalStorageUtil = require('./web/LocalStorageUtil');
-    SessionStorageUtil = require('./web/SessionStorageUtil');
+if (typeof window !== "undefined") {
+    BrowserUtil = require("./web/BrowserUtil");
+    ClipboardUtil = require("./web/ClipboardUtil");
+    LocalStorageUtil = require("./web/LocalStorageUtil");
+    SessionStorageUtil = require("./web/SessionStorageUtil");
 } else {
     BrowserUtil = null as any;
     ClipboardUtil = null as any;
@@ -47,15 +47,15 @@ if (typeof window !== 'undefined') {
 
 try {
     // Only if react can be resolved.
-    if (require.resolve('react')) {
-        ReactUtil = require('./core/ReactUtil');
-        useAPI = require('./hooks/useAPI');
-        useBool = require('./hooks/useBool');
-        useDidMountEffect = require('./hooks/useDidMountEffect');
-        useForceUpdate = require('./hooks/useForceUpdate');
-        usePrevious = require('./hooks/usePrevious');
-        useWhyDidYouUpdate = require('./hooks/useWhyDidYouUpdate');
-        useWillUnmountEffect = require('./hooks/useWillUnmountEffect');
+    if (require.resolve("react")) {
+        ReactUtil = require("./core/ReactUtil");
+        useAPI = require("./hooks/useAPI");
+        useBool = require("./hooks/useBool");
+        useDidMountEffect = require("./hooks/useDidMountEffect");
+        useForceUpdate = require("./hooks/useForceUpdate");
+        usePrevious = require("./hooks/usePrevious");
+        useWhyDidYouUpdate = require("./hooks/useWhyDidYouUpdate");
+        useWillUnmountEffect = require("./hooks/useWillUnmountEffect");
     }
 } catch (error) {
     ReactUtil = null as any;
