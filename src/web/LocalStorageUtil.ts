bf29d8aa7d67d14e72e29b424565b6c8b1f471ee
Jamyth <jamyth0712@gmail.com>
2024-05-19 01:42:14 +0800

    refactor: root - fix build config issue

--- src/web/LocalStorageUtil.ts
+++ src/web/LocalStorageUtil.ts
@@ -1,3 +1,3 @@
-import { StorageHelper } from "./internal/StorageHelper";
+import { StorageHelper } from "./internal/StorageHelper.js";
 
 export const LocalStorageUtil = new StorageHelper(localStorage);
